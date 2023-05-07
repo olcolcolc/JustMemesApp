17242f0234a1fb5782cb4cc6bc8e9a5b1cdbc760
Ola Czyrnek <aleksandra@czyrnek.net>
2023-05-07 16:46:56 +0200

    made basic, and styling

--- src/components/meme-card/MemeCard.tsx
+++ src/components/meme-card/MemeCard.tsx
@@ -1,16 +1,62 @@
 import * as React from 'react';
+import axios from 'axios';
 import "./MemeCard.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
+interface Meme {
+  id: number;
+  url: string;
+  likes: number;
+  dislikes: number;
+}
 
 interface MemeCardProps {}
 
 const MemeCard: React.FC<MemeCardProps> = () => {
+  const [memes, setMemes] = React.useState<Meme[]>([]);
+
+  React.useEffect(() => {
+    axios.get('https://raw.githubusercontent.com/olcolcolc/JustMemesApp/master/public/memes.json')
+      .then(response => {
+        setMemes(response.data.memes);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
+    //Cointains all meme cards
     <div className='memeContainer'>
+      {memes.map(meme => (
+
+        //meme card
+        <div key={meme.id} className='memeCard'>
+
+          {/* meme image */}
+          <img className='memeCard__img' src={meme.url} alt='meme' />
+
+          {/* container with vote buttons and votes */}
+          <div className='memeCard__votes'>
+            
+            {/* like */}
+            <button className='memeCard__votes-likesBtn'>
+              <FontAwesomeIcon icon={faThumbsUp} /></button>
+            <span className='memeCard__votes-likes'>
+              {meme.likes}</span>
+
+            {/* dislike */}
+            <button className='memeCard__votes-dislikesBtn'>
+              <FontAwesomeIcon icon={faThumbsDown} /></button>
+            <span className='memeCard__votes-likes'>
+              {meme.dislikes}</span>
+              
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
-
-
 
 export default MemeCard;
