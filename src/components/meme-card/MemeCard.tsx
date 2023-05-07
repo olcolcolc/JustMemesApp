e6d1014f679b12da6bd5d9fb80457f07efc5d966
Ola Czyrnek <aleksandra@czyrnek.net>
2023-05-07 18:04:48 +0200

    made voting logic without posting to api

--- src/components/meme-card/MemeCard.tsx
+++ src/components/meme-card/MemeCard.tsx
@@ -10,7 +10,9 @@ interface Meme {
   likes: number
 }
 
-interface MemeCardProps {}
+interface MemeCardProps {
+  className?: string;
+}
 
 const MemeCard: React.FC<MemeCardProps> = () => {
   const [memes, setMemes] = React.useState<Meme[]>([]);
@@ -25,6 +27,42 @@ const MemeCard: React.FC<MemeCardProps> = () => {
       });
   }, []);
 
+
+
+ //param id - ID of the meme to vote on.
+ //param vote - either "+" for upvote or "-" for downvote.
+const handleVote = (id: number, vote: "+" | "-") => {
+  // Update the state of the memes array
+  setMemes(prevMemes => {
+    // Create a new copy of the memes array (newMemes)
+    const newMemes = [...prevMemes];
+    // Find the index of the meme with the matching ID
+    const memeIndex = newMemes.findIndex(meme => meme.id === id);
+    // If the vote is an upvote, increment
+    if (vote === "+") {
+      newMemes[memeIndex].likes++;
+    // If the vote is a downvote, decrement
+    } else if (vote === "-") {
+      newMemes[memeIndex].likes--;
+    }
+
+      // Update rating in the API
+      // axios.put(`https://example.com/memes/${id}`, {
+      //   likes: newMemes[memeIndex].likes
+      // })
+      //   .then(response => {
+      //     console.log(response);
+      //   })
+      //   .catch(error => {
+      //     console.error(error);
+      //   });
+
+    // Return the updated memes array to set the new state
+      return newMemes;
+    });
+  };
+  
+  
   return (
     //Cointains all meme cards
     <div className='memeContainer'>
@@ -40,14 +78,16 @@ const MemeCard: React.FC<MemeCardProps> = () => {
           <div className='memeCard__votes'>
             
             {/* like */}
-            <button className='memeCard__votes-likesBtn'>
+            <button className='memeCard__votes-likeBtn'
+                    onClick={() => handleVote(meme.id, "+")}>
               <FontAwesomeIcon icon={faThumbsUp} /></button>
 
             <div className='memeCard__votes-likes'>
               {meme.likes}</div>
 
             {/* dislike */}
-            <button className='memeCard__votes-dislikesBtn'>
+            <button className='memeCard__votes-dislikeBtn'
+                    onClick={() => handleVote(meme.id, "-")}>
               <FontAwesomeIcon icon={faThumbsDown} /></button>
 
           </div>
