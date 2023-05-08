7da6f27279d6db5ceb3ea6771fd06207b7649269
Ola Czyrnek <aleksandra@czyrnek.net>
2023-05-08 20:25:18 +0200

    made landing-page

---
+++ src/pages/landing-page/LandingPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import MemeCard from '../../components/meme-card/MemeCard';
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className='landingPage'>
+      <MemeCard />
+    </div>
+  );
+};
+
+export default LandingPage;
